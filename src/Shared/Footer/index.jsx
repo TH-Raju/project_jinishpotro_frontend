1a9b9f4d396bf8904fd116eb0064a7c06d1884d0
TH-Raju <rajukhan894200@gmail.com>
2023-11-20 08:17:50 +0600

    Footer section design and implement

--- src/Shared/Footer/index.jsx
+++ src/Shared/Footer/index.jsx
@@ -8,6 +8,7 @@ import Cookies from "universal-cookie";
 import { Link, useNavigate } from "react-router-dom";
 import CardData from "../../Components/CardData";
 import FooterData from "../../Components/FooterData";
+import ActualFooter from "../ActualFooter";
 
 const Footer = () => {
   const { siteName, theme, setTheme } = useContext(ContextData);
@@ -85,135 +86,138 @@ const Footer = () => {
     </>
   );
   return (
-    <div className="lg:hidden text-black">
-      <div className={`navbar bg-gray-400 bottom-0 fixed lg:hidden pb-5`}>
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm -mt-48  dropdown-content  z-[1] p-2 shadow bg-gray-500 rounded-box w-52 text-white"
-            >
-              {menuItems}
-            </ul>
-          </div>
-          <Link to={"/"} className="btn btn-ghost normal-case text-xl -ml-6">
-            {siteName}
-          </Link>
-        </div>
-        <div className="navbar-end flex items-center gap-3">
-          <div
-            className={`btn btn-sm  ${
-              theme
-                ? "bg-black text-white hover:bg-gray-800 duration-200"
-                : "bg-white hover:bg-gray-400 duration-200 hover:text-white"
-            }`}
-            onClick={() => setTheme(!theme)}
-          >
-            {theme ? (
-              <p className="flex justify-center items-center ">
-                {" "}
-                <SunIcon className="h-6 w-6 text-blue-500" />
-                <p>Dark</p>
-              </p>
-            ) : (
-              <p className="flex justify-center items-center ">
-                {" "}
-                <SunIcon className="h-6 w-6 text-blue-500" />
-                <p>Light</p>
-              </p>
-            )}
-          </div>
-
-          <div>
-            <div className="drawer drawer-end z-40 ">
-              <input
-                id="my-drawer-45"
-                type="checkbox"
-                className="drawer-toggle"
-              />
-              <label
-                onClick={() => openModal()}
-                htmlFor="my-drawer-45"
-                className="btn drawer-button lg:hidden border border-none bg-transparent"
-              >
-                <h2 className="card-title relative">
-                  <ShoppingCartIcon className="h-6 w-6  text-blue-500" />
-                </h2>
-                <div className="badge badge-secondary badge-sm mb-8 ml-10 absolute">
-                  {totalData ? totalData : 0}
-                </div>
-              </label>
-              {isModalVisible && (
-                <FooterData
-                  // pass any necessary props to your modal component
-                  closeModal={closeModal}
-                />
-              )}
-              {/* <FooterData /> */}
-            </div>
-          </div>
-
-          {userEmail && (
-            <div className="dropdown dropdown-top dropdown-end">
-              <label tabIndex={0} className=" m-1">
-                <div className="avatar online mt-1">
-                  <div className="w-10 rounded-full">
-                    <img
-                      src={photo ? photo : defaultPic}
-                      className="object-cover"
-                    />
-                  </div>
-                </div>
+    <div>
+      <ActualFooter/>
+      <div className="lg:hidden text-black">
+        <div className={`navbar bg-gray-400 bottom-0 fixed lg:hidden pb-5`}>
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
               </label>
               <ul
                 tabIndex={0}
-                className="dropdown-content -ml-28  z-[1] menu p-2 shadow bg-base-100 rounded-box w-40"
+                className="menu menu-sm -mt-48  dropdown-content  z-[1] p-2 shadow bg-gray-500 rounded-box w-52 text-white"
               >
-                {/* <Link to={`/profile`}>
+                {menuItems}
+              </ul>
+            </div>
+            <Link to={"/"} className="btn btn-ghost normal-case text-xl -ml-6">
+              {siteName}
+            </Link>
+          </div>
+          <div className="navbar-end flex items-center gap-3">
+            <div
+              className={`btn btn-sm  ${
+                theme
+                  ? "bg-black text-white hover:bg-gray-800 duration-200"
+                  : "bg-white hover:bg-gray-400 duration-200 hover:text-white"
+              }`}
+              onClick={() => setTheme(!theme)}
+            >
+              {theme ? (
+                <p className="flex justify-center items-center ">
+                  {" "}
+                  <SunIcon className="h-6 w-6 text-blue-500" />
+                  <p>Dark</p>
+                </p>
+              ) : (
+                <p className="flex justify-center items-center ">
+                  {" "}
+                  <SunIcon className="h-6 w-6 text-blue-500" />
+                  <p>Light</p>
+                </p>
+              )}
+            </div>
+
+            <div>
+              <div className="drawer drawer-end z-40 ">
+                <input
+                  id="my-drawer-45"
+                  type="checkbox"
+                  className="drawer-toggle"
+                />
+                <label
+                  onClick={() => openModal()}
+                  htmlFor="my-drawer-45"
+                  className="btn drawer-button lg:hidden border border-none bg-transparent"
+                >
+                  <h2 className="card-title relative">
+                    <ShoppingCartIcon className="h-6 w-6  text-blue-500" />
+                  </h2>
+                  <div className="badge badge-secondary badge-sm mb-8 ml-10 absolute">
+                    {totalData ? totalData : 0}
+                  </div>
+                </label>
+                {isModalVisible && (
+                  <FooterData
+                    // pass any necessary props to your modal component
+                    closeModal={closeModal}
+                  />
+                )}
+                {/* <FooterData /> */}
+              </div>
+            </div>
+
+            {userEmail && (
+              <div className="dropdown dropdown-top dropdown-end">
+                <label tabIndex={0} className=" m-1">
+                  <div className="avatar online mt-1">
+                    <div className="w-10 rounded-full">
+                      <img
+                        src={photo ? photo : defaultPic}
+                        className="object-cover"
+                      />
+                    </div>
+                  </div>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="dropdown-content -ml-28  z-[1] menu p-2 shadow bg-base-100 rounded-box w-40"
+                >
+                  {/* <Link to={`/profile`}>
                 <li className=" mb-2">
                   <button className="hover:bg-primary hover:text-white my-2 bg-sky-500  rounded-lg duration-100">
                     Profile
                   </button>
                 </li>
               </Link> */}
-                <Link to={`/dashboard`}>
-                  <li className=" mb-2">
-                    <button className="hover:bg-primary hover:text-white my-2 bg-sky-500  rounded-lg duration-100">
-                      Dashboard
+                  <Link to={`/dashboard`}>
+                    <li className=" mb-2">
+                      <button className="hover:bg-primary hover:text-white my-2 bg-sky-500  rounded-lg duration-100">
+                        Dashboard
+                      </button>
+                    </li>
+                  </Link>
+
+                  <li className="mb-2">
+                    <button
+                      onClick={() => handleLogout()}
+                      className="hover:bg-red-700 hover:text-white bg-red-500    rounded-lg duration-100"
+                    >
+                      <span>Log out</span>
                     </button>
                   </li>
-                </Link>
-
-                <li className="mb-2">
-                  <button
-                    onClick={() => handleLogout()}
-                    className="hover:bg-red-700 hover:text-white bg-red-500    rounded-lg duration-100"
-                  >
-                    <span>Log out</span>
-                  </button>
-                </li>
-              </ul>
-            </div>
-          )}
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{menuItems}</ul>
+                </ul>
+              </div>
+            )}
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{menuItems}</ul>
+          </div>
         </div>
       </div>
     </div>
