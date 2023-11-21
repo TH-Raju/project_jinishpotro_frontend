1ee6246241da71a07c63d903029a543f5638601a
TH-Raju <rajukhan894200@gmail.com>
2023-11-21 10:27:42 +0600

    Discount Product card add

---
+++ src/Components/DiscountProdCard/index.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+
+import { useContext } from "react";
+import { ContextData } from "../../Context";
+
+// eslint-disable-next-line react/prop-types
+const DiscountProdCard = ({ products }) => {
+  const { name, price, detail, photo, discount } = products;
+  const { theme } = useContext(ContextData);
+
+  return (
+    <div>
+      <div className="card card-compact text-black w-40 md:w-52 bg-base-100 shadow-xl border border-blue-200 p-2">
+        <figure>
+          <img src={photo} alt="Shoes" className="relative h-16 md:h-20" />
+          <div className="badge badge-secondary absolute top-0 right-0">
+            {discount}%
+          </div>
+        </figure>
+        <div className="card-body">
+          <h2 className="md:card-title h-14 font-bold text-base w-[90%] leading-5">
+            {name}
+          </h2>
+          {/* <p>
+            {" "}
+            Price: <span className="font-bold">{price}</span>
+          </p> */}
+        </div>
+        <div className="">
+          <button className="btn btn-outline btn-info btn-sm w-full ">
+            Price <span className="font-bold">{price}</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DiscountProdCard;
